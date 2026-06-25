2387055e39663dc0c5dae58522e4dd1156e60be3
sahil <sahil@example.com>
2026-06-25 10:08:58 +0000

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,4 +36,15 @@ output "bastion_public_ip" {
   description = "Exposes the child module bastion IP directly to the Jenkins runner"
 }
 
+output "master_private_ip" {
+  value       = aws_instance.postgres_master.private_ip
+  description = "The private IP address of the primary database cluster"
+}
 
+output "replica_1_private_ip" {
+  value       = aws_instance.postgres_replica_1.private_ip
+}
+
+output "replica_2_private_ip" {
+  value       = aws_instance.postgres_replica_2.private_ip
+}
